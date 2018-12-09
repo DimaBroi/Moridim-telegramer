f3a8a69eebe1e91cc1799e2fa1585de3e60c2d07
DimaBroi <31161769+dimabroi@users.noreply.github.com>
2018-12-09 18:46:07 +0200

    Fixed an issu with user cred

--- nitrobit_psw.py
+++ nitrobit_psw.py
@@ -1,5 +1,6 @@
 import usr #remove
 
-class nitrobit_psw:
+#class nitrobit_psw: #Seems to be used as nitrobitpsw
+class nitrobitpsw:
     email=usr.email #fill you email
     psw=usr.psw #fill you password
